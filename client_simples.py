2cec276fea91573ad523d10125e1aceb510cf3b1
Mariana Romariz <marianaromariz@outlook.com>
2019-04-25 12:38:15 -0300

    Update client_simples.py

--- client_simples.py
+++ client_simples.py
@@ -3,18 +3,13 @@ from socket import *
 text = input("Mensagem:")
 
 if(text != ''):
-
-	s = socket()
-
 	meusbytes = str.encode(text, "UTF-8")
 	print(meusbytes)
-
+	s = socket()
 	s.connect(("192.168.0.20",8752))
 	s.send(meusbytes)
 	data = s.recv (1024)
 	print (data.decode("utf-8"))
-
 	s.close()
-
 else:
 	print("Digite algo")
