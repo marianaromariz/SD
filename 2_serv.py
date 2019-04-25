122ec3a5f9a50bbe2c0d91e5c538b46786c7cfd4
Mariana Romariz <marianaromariz@outlook.com>
2019-04-25 10:35:54 -0300

    Update 2_serv.py

--- 2_serv.py
+++ 2_serv.py
@@ -4,30 +4,19 @@ from threading import Thread
 def atende (conn, cliente):
         while True:
                 data = conn.recv (4096)
-
                 if not data or len(data) == 0:
                         break
-
                 print (str(cliente)+" me mandou "+data.decode("utf-8") )
-
                 conn.send (str.encode ("Eu sei que voce me mandou "+data.decode("utf-8") , "UTF-8"))
-
         print ("Fim da conexao com "+str(cliente))
-
         conn.close ()
-        
-
+     
 s = socket ()
-
-host = "192.168.0.20"
-porta = 8752
-s.bind ((host, porta))
+s.bind (("192.168.0.20", 8752))
 s.listen (10)
 nthr = 0
-
 while True:
         (conn, cliente) = s.accept ()
-
         print ("Recebi a conexao de "+str(cliente))
         nthr += 1
         print ("Criando thread "+str(nthr))
