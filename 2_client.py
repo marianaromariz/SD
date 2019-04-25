96456ad3f0c334c3b84850a42e69fd9c0ce93788
Mariana Romariz <marianaromariz@outlook.com>
2019-04-25 10:34:33 -0300

    Update 2_client.py

--- 2_client.py
+++ 2_client.py
@@ -1,17 +1,13 @@
 from socket import  *
 
-s = socket ()
-
 minhastr = "Ola mundo"
 print (minhastr)
-meusbytes=str.encode (minhastr, "UTF-8")
+meusbytes = str.encode (minhastr, "UTF-8")
 print (meusbytes)
 
+s = socket ()
 s.connect(("192.168.0.20", 8752))
 s.send (meusbytes)
-
 data = s.recv (1024)
-
 print (data.decode("utf-8"))
-
 s.close ()
